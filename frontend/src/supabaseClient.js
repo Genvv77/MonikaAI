50915101dd595145209fbe2e7a4f321db8eb155c
Genvv77 <genvism@gmail.com>
2025-12-25 23:16:08 +0100

    Secure backend verification implemented

--- frontend/src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Replace these with the values you copied from Supabase Settings -> API
-const supabaseUrl = 'https://aconfqrhkuspcpbpfvav.supabase.co'
-const supabaseKey = 'sb_publishable_Pwd0N6rexQuZR6GCzmIbNw_WUXOfS8o'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
